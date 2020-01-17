af93d7bc06b14f677929abdf1a174a561486b6db
Abhinandan Mittal <41774513+abhimittal342@users.noreply.github.com>
2020-01-17 16:48:46 +0530

    navbar fixed

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-jQuery(document).ready(function($) {
-  // Scroll to the desired section on click
-  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
-  // Example: 
-  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
-  function scrollToSection(event) {
-    event.preventDefault();
-    var $section = $($(this).attr('href')); 
-    $('html, body').animate({
-      scrollTop: $section.offset().top
-    }, 500);
-  }
+$(document).ready(function(){
+  // Add smooth scrolling to all links
   
-}(jQuery));
+
+  $("a").on('click', function(event) {
+    
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
